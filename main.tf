6e909464ffb955fa915fc6bd5510e9d1117b384b
SarahAllett <alletts@parliament.uk>
2021-02-04 21:45:56 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -132,7 +132,8 @@ data "aws_vpc_endpoint_service" "s3" {
 resource "aws_vpc_endpoint" "s3" {
   count = "${var.enable_s3_endpoint}"
 
-  vpc_id       = "${aws_vpc.mod.id}"
+//  vpc_id       = "${aws_vpc.mod.id}"
+  vpc_id       = "${aws_vpc.mod[count.index]}"
   service_name = "com.amazonaws.eu-west-1.s3"
   //service_name = "${data.aws_vpc_endpoint_service.s3.service_name}"
 }
@@ -148,8 +149,10 @@ resource "aws_vpc_endpoint_route_table_association" "private_s3" {
 resource "aws_vpc_endpoint_route_table_association" "public_s3" {
   count = "${var.enable_s3_endpoint ? length(var.public_subnets) : 0}"
 
+  //vpc_endpoint_id = "${aws_vpc_endpoint.s3[count.index]}"
   vpc_endpoint_id = "${aws_vpc_endpoint.s3.id}"
-  route_table_id  = "${aws_route_table.public.id}"
+  //route_table_id  = "${aws_route_table.public.id}"
+  route_table_id  = "${aws_route_table.public[count.index]}"
 }
 
 data "aws_vpc_endpoint_service" "dynamodb" {
@@ -166,7 +169,8 @@ resource "aws_vpc_endpoint" "dynamodb" {
 resource "aws_vpc_endpoint_route_table_association" "private_dynamodb" {
   count = "${var.enable_dynamodb_endpoint ? length(var.private_subnets) : 0}"
 
-  vpc_endpoint_id = "${aws_vpc_endpoint.dynamodb.id}"
+  //vpc_endpoint_id = "${aws_vpc_endpoint.dynamodb.id}"
+  vpc_endpoint_id = "${aws_vpc_endpoint.dynamodb[count.index]}"
   route_table_id  = "${element(aws_route_table.private.*.id, count.index)}"
 }
 
