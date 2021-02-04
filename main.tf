8584de63816681744298014628d1fad9d2a4398e
SarahAllett <alletts@parliament.uk>
2021-02-04 23:19:15 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -123,33 +123,33 @@ resource "aws_nat_gateway" "natgw" {
   depends_on = ["aws_internet_gateway.mod"]
 }
 
-data "aws_vpc_endpoint_service" "s3" {
-  service = "s3"
+//data "aws_vpc_endpoint_service" "s3" {
+  //service = "s3"
   //service_type = "Gateway"
-}
+//}
 
-resource "aws_vpc_endpoint" "s3" {
-  count = "${var.enable_s3_endpoint}"
+//resource "aws_vpc_endpoint" "s3" {
+  //count = "${var.enable_s3_endpoint}"
 
 
-vpc_id       = "${aws_vpc.mod.id}"
-  service_name = "com.amazonaws.eu-west-1.s3"
+//vpc_id       = "${aws_vpc.mod.id}"
+  //service_name = "com.amazonaws.eu-west-1.s3"
   //service_name = "${data.aws_vpc_endpoint_service.s3.service_name}"
-}
+//}
 
-resource "aws_vpc_endpoint_route_table_association" "private_s3" {
-  count = "${var.enable_s3_endpoint ? length(var.private_subnets) : 0}"
+//resource "aws_vpc_endpoint_route_table_association" "private_s3" {
+  //count = "${var.enable_s3_endpoint ? length(var.private_subnets) : 0}"
 
-  vpc_endpoint_id = "${aws_vpc_endpoint.s3.id}"
-  route_table_id  = "${element(aws_route_table.private.*.id, count.index)}"
-}
+  //vpc_endpoint_id = "${aws_vpc_endpoint.s3.id}"
+  //route_table_id  = "${element(aws_route_table.private.*.id, count.index)}"
+//}
 
-resource "aws_vpc_endpoint_route_table_association" "public_s3" {
-  count = "${var.enable_s3_endpoint ? length(var.public_subnets) : 0}"
+//resource "aws_vpc_endpoint_route_table_association" "public_s3" {
+  //count = "${var.enable_s3_endpoint ? length(var.public_subnets) : 0}"
 
-  vpc_endpoint_id = "${aws_vpc_endpoint.s3.id}"
-  route_table_id  = "${aws_route_table.public.id}"
-}
+  //vpc_endpoint_id = "${aws_vpc_endpoint.s3.id}"
+  //route_table_id  = "${aws_route_table.public.id}"
+//}
 
 data "aws_vpc_endpoint_service" "dynamodb" {
   service = "dynamodb"
